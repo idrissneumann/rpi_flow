26ba35f39fd7cf9721c16c629561e6b16362ac46
Idriss Neumann <neumann.idriss@gmail.com>
2022-02-11 13:13:32 +0000

    Add bitbucket mirror dans change base

--- ci/mirror.sh
+++ ci/mirror.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-REPO_PATH="/home/centos/flow/"
+REPO_PATH="${PROJECT_HOME}/flow/"
 
 cd "${REPO_PATH}" && git pull origin main || :
 git push github main
 git push pgitlab main
+git push bitbucket main
 exit 0
